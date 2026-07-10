53c1fe1c297d2969ea3e579139f319990edf6271
Trishan Shrestha <trishanshrestha03@gmail.com>
2026-07-10 11:11:20 +0545

    fix: added boolean to filter what gets inserted in history

--- maintenance/compaction.py
+++ maintenance/compaction.py
@@ -177,7 +177,7 @@ def run_maintenance(spark, table_name):
     # BEFORE
     # ----------------------------------------------------------
 
-    before = get_table_health(spark, table_name, event_type="maintenance_before")
+    before = get_table_health(spark, table_name, event_type="maintenance_before", record_history = True)
 
     print("\nBefore Maintenance")
     print_report(before)
@@ -244,7 +244,7 @@ def run_maintenance(spark, table_name):
     # AFTER
     # ----------------------------------------------------------
 
-    after = get_table_health(spark, table_name, event_type="maintenance_after")
+    after = get_table_health(spark, table_name, event_type="maintenance_after", record_history = True)
 
     print("\nAfter Maintenance")
     print_report(after)
